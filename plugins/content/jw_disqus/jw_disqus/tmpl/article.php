e411561f3ddfd66aee9afa4ee761ab11f851ef8b
Fotis Evangelou <fevangelou@users.noreply.github.com>
2018-09-21 12:42:23 +0300

    Remove unneeded string

--- plugins/content/jw_disqus/jw_disqus/tmpl/article.php
+++ plugins/content/jw_disqus/jw_disqus/tmpl/article.php
@@ -18,7 +18,9 @@ defined('_JEXEC') or die;
 <!-- DISQUS comments counter and anchor link -->
 <div class="jwDisqusArticleCounter">
     <span>
-        <a class="jwDisqusArticleCounterLink" href="#disqus_thread" data-disqus-identifier="<?php echo $output->disqusIdentifier; ?>"><?php echo JText::_("JW_DISQUS_VIEW_COMMENTS"); ?></a>
+        <a class="jwDisqusArticleCounterLink" href="#disqus_thread" data-disqus-identifier="<?php echo $output->disqusIdentifier; ?>">
+            <?php echo JText::_("JW_DISQUS_VIEW_COMMENTS"); ?>
+        </a>
     </span>
     <div class="clr"></div>
 </div>
@@ -30,9 +32,6 @@ defined('_JEXEC') or die;
 <div class="jwDisqusForm">
     <?php echo $output->comments; ?>
     <div id="jwDisqusFormFooter">
-        <a target="_blank" href="https://disqus.com" class="dsq-brlink">
-            <?php echo JText::_("JW_DISQUS_BLOG_COMMENTS_POWERED_BY"); ?> <span class="logo-disqus">DISQUS</span>
-        </a>
         <a id="jwDisqusBackToTop" href="#startOfPage">
             <?php echo JText::_("JW_DISQUS_BACK_TO_TOP"); ?>
         </a>
