64f6d509bdb2ea613a5b18c82ddf720d5e02f083
Fotis Evangelou <fevangelou@users.noreply.github.com>
2018-09-21 13:46:45 +0300

    Remove deprecated data attributes

--- plugins/content/jw_disqus/jw_disqus/tmpl/listing.php
+++ plugins/content/jw_disqus/jw_disqus/tmpl/listing.php
@@ -15,6 +15,6 @@ defined('_JEXEC') or die;
 <?php echo $row->text; ?>
 
 <!-- DISQUS comments counter and anchor link -->
-<a class="jwDisqusListingCounterLink" href="<?php echo $output->itemURL; ?>#disqus_thread" title="<?php echo JText::_("JW_DISQUS_ADD_A_COMMENT"); ?>" data-disqus-identifier="<?php echo $output->disqusIdentifier; ?>">
+<a class="jwDisqusListingCounterLink" href="<?php echo $output->itemURL; ?>#disqus_thread" title="<?php echo JText::_("JW_DISQUS_ADD_A_COMMENT"); ?>">
     <?php echo JText::_("JW_DISQUS_ADD_A_COMMENT"); ?>
 </a>
