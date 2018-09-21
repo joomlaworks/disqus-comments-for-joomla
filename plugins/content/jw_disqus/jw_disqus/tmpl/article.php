f7e2421ce03499f5078b4ebb4b521b36f09b3a67
Fotis Evangelou <fevangelou@users.noreply.github.com>
2018-09-21 12:17:53 +0300

    PSR-1/2 formatting

--- plugins/content/jw_disqus/jw_disqus/tmpl/article.php
+++ plugins/content/jw_disqus/jw_disqus/tmpl/article.php
@@ -17,10 +17,10 @@ defined('_JEXEC') or die;
 <?php if($disqusArticleCounter): ?>
 <!-- DISQUS comments counter and anchor link -->
 <div class="jwDisqusArticleCounter">
-	<span>
-		<a class="jwDisqusArticleCounterLink" href="#disqus_thread" data-disqus-identifier="<?php echo $output->disqusIdentifier; ?>"><?php echo JText::_("JW_DISQUS_VIEW_COMMENTS"); ?></a>
-	</span>
-	<div class="clr"></div>
+    <span>
+        <a class="jwDisqusArticleCounterLink" href="#disqus_thread" data-disqus-identifier="<?php echo $output->disqusIdentifier; ?>"><?php echo JText::_("JW_DISQUS_VIEW_COMMENTS"); ?></a>
+    </span>
+    <div class="clr"></div>
 </div>
 <?php endif; ?>
 
@@ -28,16 +28,16 @@ defined('_JEXEC') or die;
 
 <!-- DISQUS comments block -->
 <div class="jwDisqusForm">
-	<?php echo $output->comments; ?>
-	<div id="jwDisqusFormFooter">
-		<a target="_blank" href="http://disqus.com" class="dsq-brlink">
-			<?php echo JText::_("JW_DISQUS_BLOG_COMMENTS_POWERED_BY"); ?> <span class="logo-disqus">DISQUS</span>
-		</a>
-		<a id="jwDisqusBackToTop" href="#startOfPage">
-			<?php echo JText::_("JW_DISQUS_BACK_TO_TOP"); ?>
-		</a>
-		<div class="clr"></div>
-	</div>
+    <?php echo $output->comments; ?>
+    <div id="jwDisqusFormFooter">
+        <a target="_blank" href="http://disqus.com" class="dsq-brlink">
+            <?php echo JText::_("JW_DISQUS_BLOG_COMMENTS_POWERED_BY"); ?> <span class="logo-disqus">DISQUS</span>
+        </a>
+        <a id="jwDisqusBackToTop" href="#startOfPage">
+            <?php echo JText::_("JW_DISQUS_BACK_TO_TOP"); ?>
+        </a>
+        <div class="clr"></div>
+    </div>
 </div>
 
 <div class="clr"></div>
--- plugins/content/jw_disqus/jw_disqus/tmpl/listing.php
+++ plugins/content/jw_disqus/jw_disqus/tmpl/listing.php
@@ -16,5 +16,5 @@ defined('_JEXEC') or die;
 
 <!-- DISQUS comments counter and anchor link -->
 <a class="jwDisqusListingCounterLink" href="<?php echo $output->itemURL; ?>#disqus_thread" title="<?php echo JText::_("JW_DISQUS_ADD_A_COMMENT"); ?>" data-disqus-identifier="<?php echo $output->disqusIdentifier; ?>">
-	<?php echo JText::_("JW_DISQUS_ADD_A_COMMENT"); ?>
+    <?php echo JText::_("JW_DISQUS_ADD_A_COMMENT"); ?>
 </a>
